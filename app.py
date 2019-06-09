d76bd7fe4ddb42f9425238f08c72ed0b7841c673
Andrew Jarombek <ajarombek95@gmail.com>
2019-06-09 10:15:54 -0400

    barebones python flask api

---
+++ app.py
@@ -0,0 +1,58 @@
+"""
+Entry point to the SaintsXCTF API.  The API uses the flask library.
+Author: Andrew Jarombek
+Date: 6/8/2019
+"""
+
+from flask import Flask, jsonify
+
+app = Flask('saints-xctf-api')
+
+version_number = 2
+
+
+@app.route('/', methods=['GET'])
+def api():
+    """
+    Entry point for the SaintsXCTF API
+    :return: A JSON welcome message
+    """
+    return jsonify({
+        'api': 'saints-xctf',
+        'version': 2.0,
+        'message': 'Welcome to the saints-xctf API!'
+    })
+
+
+@app.errorhandler(404)
+def error_404(ex):
+    return jsonify({
+        'api': 'saints-xctf',
+        'version': 2.0,
+        'error_description': "Page Not Found",
+        'error_code': 404,
+        'exception': str(ex)
+    }), 404
+
+
+@app.errorhandler(500)
+def error_500(ex):
+    return jsonify({
+        'api': 'saints-xctf',
+        'version': 2.0,
+        'error_description': "Internal Server Error",
+        'error_code': 500,
+        'exception': str(ex)
+    }), 500
+
+
+@app.route(f'/v{version_number}/', methods=['GET'])
+def version():
+    return jsonify({
+        'api': 'saints-xctf',
+        'version': 2.0,
+        'message': f'This endpoint is for version {version_number} of the API.  This is the latest version'
+    })
+
+
+app.run(port=8081)
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Setup commands for the API
+# Author: Andrew Jarombek
+# Date: 6/8/2019
+
+# Install flask
+pip3 --version
+pip3 install flask
+
+# Run the app from a development server
+export FLASK_APP=app.py
+python3 -m flask run
