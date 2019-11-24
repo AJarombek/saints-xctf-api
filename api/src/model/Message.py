874203b5b067438f8721ff91d73359d45f32adf6
Andrew Jarombek <ajarombek95@gmail.com>
2019-11-24 11:17:56 -0500

    refactor message feed api

--- api/src/model/Message.py
+++ api/src/model/Message.py
@@ -10,21 +10,32 @@ from sqlalchemy import Column
 
 class Message(db.Model):
 
-    def __init__(self, log: dict):
+    def __init__(self, message: dict):
         """
         Initialize a Message by passing in a dictionary.
-        :param log: A dictionary with fields matching the Message fields
+        :param message: A dictionary with fields matching the Message fields
         """
-        self.message_id = log.get('message_id')
-        self.username = log.get('username')
-        self.first = log.get('first')
-        self.last = log.get('last')
-        self.group_name = log.get('group_name')
-        self.time = log.get('time')
-        self.content = log.get('content')
+        self.message_id = message.get('message_id')
+        self.username = message.get('username')
+        self.first = message.get('first')
+        self.last = message.get('last')
+        self.group_name = message.get('group_name')
+        self.time = message.get('time')
+        self.content = message.get('content')
+        self.deleted = message.get('deleted')
+        self.created_date = message.get('created_date')
+        self.created_user = message.get('created_user')
+        self.created_app = message.get('created_app')
+        self.modified_date = message.get('modified_date')
+        self.modified_user = message.get('modified_user')
+        self.modified_app = message.get('modified_app')
+        self.deleted_date = message.get('deleted_date')
+        self.deleted_user = message.get('deleted_user')
+        self.deleted_app = message.get('deleted_app')
 
     __tablename__ = 'messages'
 
+    # Data Columns
     message_id = Column(db.INT, autoincrement=True, primary_key=True)
     username = Column(db.VARCHAR(20), db.ForeignKey('users.username'), nullable=False)
     first = Column(db.VARCHAR(30), nullable=False)
@@ -32,6 +43,58 @@ class Message(db.Model):
     group_name = Column(db.VARCHAR(20), db.ForeignKey('groups.group_name'), nullable=False)
     time = Column(db.DATETIME, nullable=False)
     content = Column(db.VARCHAR(1000))
+    deleted = Column(db.CHAR(1))
+
+    # Audit Columns
+    created_date = Column(db.DATETIME)
+    created_user = Column(db.VARCHAR(31))
+    created_app = Column(db.VARCHAR(31))
+    modified_date = Column(db.DATETIME)
+    modified_user = Column(db.VARCHAR(31))
+    modified_app = Column(db.VARCHAR(31))
+    deleted_date = Column(db.DATETIME)
+    deleted_user = Column(db.VARCHAR(31))
+    deleted_app = Column(db.VARCHAR(31))
+
+    def __str__(self):
+        """
+        String representation of a group/team message.  This representation is meant to be human readable.
+        :return: The message in string form.
+        """
+        return f'Message: [message_id: {self.message_id}, username: {self.username}, first: {self.first}, ' \
+            f'last: {self.last}, group_name: {self.group_name}, time: {self.time}, content: {self.content}, ' \
+            f'deleted: {self.deleted}]'
 
     def __repr__(self):
+        """
+        String representation of a group/team message.  This representation is meant to be machine readable.
+        :return: The message in string form.
+        """
         return '<Message %r>' % self.message_id
+
+    def __eq__(self, other):
+        """
+        Determine value equality between this object and another object.
+        :param other: Another object to compare to this group/team message.
+        :return: True if the objects are equal, False otherwise.
+        """
+        return Message.compare(self, other)
+
+    @classmethod
+    def compare(cls, message_1, message_2) -> bool:
+        """
+        Helper function used to determine value equality between two objects that are assumed to be messages.
+        :param message_1: The first message object.
+        :param message_2: The second message object.
+        :return: True if the objects are equal, False otherwise.
+        """
+        return all([
+            message_1.message_id == message_2.message_id,
+            message_1.username == message_2.username,
+            message_1.first == message_2.first,
+            message_1.last == message_2.last,
+            message_1.group_name == message_2.group_name,
+            str(message_1.time) == str(message_2.time),
+            message_1.content == message_2.content,
+            message_1.deleted == message_2.deleted
+        ])
--- api/src/route/messageFeedRoute.py
+++ api/src/route/messageFeedRoute.py
@@ -4,14 +4,14 @@ Author: Andrew Jarombek
 Date: 7/27/2019
 """
 
-from flask import Blueprint, request, jsonify, current_app
+from flask import Blueprint, request, jsonify, Response
 from dao.messageDao import MessageDao
 
 message_feed_route = Blueprint('message_feed_route', __name__, url_prefix='/v2/message_feed')
 
 
 @message_feed_route.route('/<filter_by>/<bucket>/<limit>/<offset>', methods=['GET'])
-def messages(filter_by, bucket, limit, offset):
+def message_feed(filter_by, bucket, limit, offset):
     """
     Endpoints for retrieving group messages based on filters.
     :param filter_by: The filtering mechanism for the messages.  You can filter by group (group_name).
@@ -22,40 +22,82 @@ def messages(filter_by, bucket, limit, offset):
     """
     if request.method == 'GET':
         ''' [GET] /v2/message_feed '''
-        if filter_by == 'group' or filter_by == 'groups':
-            messages = MessageDao.get_message_feed(group_name=bucket, limit=limit, offset=offset)
-        else:
-            messages = None
+        return message_feed_get(filter_by, bucket, limit, offset)
 
-        # Generate MessageFeed API URLs
-        self_url = f'/v2/message_feed/{filter_by}/{bucket}/{limit}/{offset}'
 
-        prev_offset = (offset - limit) >= 0
-        if prev_offset:
-            prev_url = f'/v2/message_feed/{filter_by}/{bucket}/{limit}/{prev_offset}'
-        else:
-            prev_url = None
+@message_feed_route.route('/links', methods=['GET'])
+def message_feed_links() -> Response:
+    """
+    Endpoint for information about the message feed API endpoints.
+    :return: Metadata about the message feed API.
+    """
+    if request.method == 'GET':
+        ''' [GET] /v2/message_feed/links '''
+        return message_feed_links_get()
 
-        if messages is None:
-            next_url = None
 
-            response = jsonify({
-                'self': self_url,
-                'next': next_url,
-                'prev': prev_url,
-                'messages': None,
-                'error': 'no messages found in this feed'
-            })
-            response.status_code = 500
-            return response
-        else:
-            next_url = f'/v2/message_feed/{filter_by}/{bucket}/{limit}/{offset + limit}'
+def message_feed_get(filter_by, bucket, limit, offset):
+    """
+    Get a list of messages based on certain filters.
+    :param filter_by: The filtering mechanism for the messages.  You can filter by group (group_name).
+    :param bucket: The bucket to filter by (the group name)
+    :param limit: The maximum number of messages to return
+    :param offset: The number of messages to skip from the result of this filter before returning
+    :return: A response object for the GET API request.
+    """
+    if filter_by == 'group' or filter_by == 'groups':
+        messages = MessageDao.get_message_feed(group_name=bucket, limit=limit, offset=offset)
+    else:
+        messages = None
 
-            response = jsonify({
-                'self': self_url,
-                'next': next_url,
-                'prev': prev_url,
-                'messages': messages
-            })
-            response.status_code = 200
-            return response
+    # Generate MessageFeed API URLs
+    self_url = f'/v2/message_feed/{filter_by}/{bucket}/{limit}/{offset}'
+
+    prev_offset = (offset - limit) >= 0
+    if prev_offset:
+        prev_url = f'/v2/message_feed/{filter_by}/{bucket}/{limit}/{prev_offset}'
+    else:
+        prev_url = None
+
+    if messages is None:
+        next_url = None
+
+        response = jsonify({
+            'self': self_url,
+            'next': next_url,
+            'prev': prev_url,
+            'messages': None,
+            'error': 'no messages found in this feed'
+        })
+        response.status_code = 500
+        return response
+    else:
+        next_url = f'/v2/message_feed/{filter_by}/{bucket}/{limit}/{offset + limit}'
+
+        response = jsonify({
+            'self': self_url,
+            'next': next_url,
+            'prev': prev_url,
+            'messages': messages
+        })
+        response.status_code = 200
+        return response
+
+
+def message_feed_links_get() -> Response:
+    """
+    Get all the other message feed API endpoints.
+    :return: A response object for the GET API request
+    """
+    response = jsonify({
+        'self': f'/v2/message_feed/links',
+        'endpoints': [
+            {
+                'link': '/v2/message_feed/<filter_by>/<bucket>/<limit>/<offset>',
+                'verb': 'GET',
+                'description': 'Get a list of group/team messages based on certain filters.'
+            }
+        ]
+    })
+    response.status_code = 200
+    return response
